955b43d698d348ed8b59b9b97948eaeb44de8778
Sebastiano Quinto <squinto@lxplus969.cern.ch>
2025-12-17 16:31:44 +0100

    Fixing phi dependence for 2023BPix

--- mkShapesRDF/processor/modules/LeptonSF.py
+++ mkShapesRDF/processor/modules/LeptonSF.py
@@ -407,6 +407,7 @@ class LeptonSF(Module):
                             evaluator = f""" 
                             pt = ROOT::VecOps::Max(ROOT::RVecF{{ROOT::VecOps::Min(ROOT::RVecF{{ele_pt[i], {self.el_maxPt}}}), {self.el_minPt}}});   
                             eta = ROOT::VecOps::Max(ROOT::RVecF{{ROOT::VecOps::Min(ROOT::RVecF{{ele_eta[i]+detasc, {self.el_maxEta}}}), {self.el_minEta}}});
+                            phi = ele_phi[i];
 
                             sf     = cset_electron_{wp}_wpSF->evaluate({{"{egamma_era}", "sf", "{label}", eta, pt, phi}});
                             sfup   = cset_electron_{wp}_wpSF->evaluate({{"{egamma_era}", "sfup", "{label}", eta, pt, phi}});
@@ -482,6 +483,7 @@ class LeptonSF(Module):
                                 evaluator_tth = f"""
                                 pt = ROOT::VecOps::Max(ROOT::RVecF{{ROOT::VecOps::Min(ROOT::RVecF{{ele_pt[i], {self.el_maxPt}}}), {self.el_minPt}}});
                                 eta = ROOT::VecOps::Max(ROOT::RVecF{{ROOT::VecOps::Min(ROOT::RVecF{{ele_eta[i]+detasc, {self.el_maxEta}}}), {self.el_minEta}}}); 
+                                phi = ele_phi[i];
 
                                 sf_tth     = cset_electron_{wp}_tthMvaSF->evaluate({{"{egamma_era}", "sf", "{label}", eta, pt, phi}});
                                 sfup_tth   = cset_electron_{wp}_tthMvaSF->evaluate({{"{egamma_era}", "sfup", "{label}", eta, pt, phi}});
