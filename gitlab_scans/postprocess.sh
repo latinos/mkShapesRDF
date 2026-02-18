f1a394bc8a84ed58cfeacf6391aeec6d41fb150e
Sebastiano Quinto <squinto@lxplus993.cern.ch>
2026-02-18 10:55:53 +0100

    Update for pipelines

---
+++ gitlab_scans/postprocess.sh
@@ -0,0 +1,4 @@
+
+
+
+mkPostProc -o 1 -p Summer22_130x_nAODv12_Full2022v12 -s MCl2loose2022v12__MCCorr2022v12JetScaling__l2tight
---
+++ gitlab_scans/scripts/histos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkShapesRDF -c 1 -o 0 -b 0
+mkPlot --onlyPlot c --showIntegralLegend 1 --fileFormats png
